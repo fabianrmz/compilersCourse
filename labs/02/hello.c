a13ccac96c4db29f12bfa059c9b28735e85acbfc
Óscar Fabián Ramírez González <A01630024@itesm.mx>
2020-09-13 20:42:39 -0500

    Solve mylab01

---
+++ labs/02/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+[]
+int foo(){
+	//this is my homework :)fge5[]{}{]}
+
+	return 3;
+
+}
+int main(){
+    for (int x = 0; x < 10;x++){
+        printf("hi\n");
+    }
+    return 0;
+}
+[{]
